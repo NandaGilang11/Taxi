081c7239464854af62deb88d8e52566b2ab5d17c
NandaGilang11 <nandagilang@students.amikom.ac.id>
2022-03-23 17:33:32 +0700

    Tugas Class Object

---
+++ TugasTaxi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek Taxi
+            Taxi taxi = new Taxi();
+            Taxi taxi2 = new Taxi();
+
+            // Properties value
+            taxi.DriverName = "Itadori";
+            taxi.OnDuty = "Yes";
+            taxi.NumPassenger = 11;
+
+            taxi2.DriverName = "Fushiguro";
+            taxi2.OnDuty = "Yes";
+            taxi2.NumPassenger = 18;
+
+            // Pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            taxi2.TaxiInfo();
+            taxi2.PickUpPassenger();
+            taxi2.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TugasTaxi/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Name Driver\t : {0}", DriverName);
+            Console.WriteLine("On Duty\t\t : {0}", OnDuty);
+            Console.WriteLine("Number Passegers : {0}", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} picking up passengers", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} carrying passengers\n", DriverName);
+        }
+    }
+}
